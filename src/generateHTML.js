6fa90af54c3b053e2aa88a2456ee9370fb821517
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-03 16:38:31 -0700

    last save

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,7 +2,14 @@
 function generateHTMl(team) {
 let array = [];
   for (let i = 0; i < team.length; i++) {
-    array.push(`<h1>${team[i].name}</h1>`)
+    array.push(`<div>
+    <h1>${team[i].name}</h1>
+    <div>
+      <p>${team[i].id}</p>
+      <p>${team[i].email}</p>
+      <p>${team[i].github || team[i].officeNumber || team[i].school}</p>
+    </div>
+  </div>`)
   }
 
   return `<!DOCTYPE html>
@@ -11,6 +18,7 @@ let array = [];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
     <title>Team</title>
   </head>
   <body>
